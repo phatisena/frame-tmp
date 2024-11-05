28a07189b1e43cc387dd4e7d893ba4cda4d41287
Phatisena <naphatism@hotmail.com>
2024-11-05 10:38:12 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,68 @@
+namespace images {
+    
+    export function drawTransparentImage(src: Image, to: Image, x: number, y: number) {
+        if (!src || !to) {
+            return;
+        }
+        to.drawTransparentImage(src, x, y);
+    }
+
+    
+    //%blockid=img_imgframe
+    //%block="Set image frame $ImgF=screen_image_picker wiodth: $Wh Height: $Ht"
+    //%group="frame image"
+    //%inlineInputMode=inline
+    //%weight=8
+    export function SetImgFrame(ImgF: Image, Wh: number, Ht: number) {
+        let ImgOutput = image.create(Wh, Ht)
+        let Twidt = Math.floor(ImgF.width / 3)
+        let Theig = Math.floor(ImgF.height / 3)
+        let ImgTable: Image[] = []
+        let Uimg: Image = null
+        let sw = 0
+        let sh = 0
+        for (let hj = 0; hj < 3; hj++) {
+            for (let wi = 0; wi < 3; wi++) {
+                Uimg = image.create(Twidt, Theig)
+                drawTransparentImage(ImgF, Uimg, 0 - wi * Twidt, 0 - hj * Theig)
+                ImgTable.push(Uimg.clone())
+            }
+        }
+        for (let wi = 0; wi < Math.floor(Wh / Twidt); wi++) {
+            for (let hj = 0; hj < Math.floor(Ht / Theig); hj++) {
+                sw = Math.min(wi * Twidt, Wh - Twidt)
+                sh = Math.min(hj * Theig, Ht - Theig)
+                if (hj == 0 && wi == 0) {
+                    drawTransparentImage(ImgTable[0], ImgOutput, sw, sh)
+                } else if (hj == Math.floor(Ht / Theig) - 1 && wi == Math.floor(Wh / Twidt) - 1) {
+                    drawTransparentImage(ImgTable[8], ImgOutput, sw, sh)
+                } else if (hj == Math.floor(Ht / Theig) - 1 && wi == 0) {
+                    drawTransparentImage(ImgTable[6], ImgOutput, sw, sh)
+                } else if (hj == 0 && wi == Math.floor(Wh / Twidt) - 1) {
+                    drawTransparentImage(ImgTable[2], ImgOutput, sw, sh)
+                } else {
+                    if (wi > 0 && wi < Math.floor(Wh / Twidt) - 1) {
+                        if (hj == 0) {
+                            drawTransparentImage(ImgTable[1], ImgOutput, sw, sh)
+                        } else if (hj == Math.floor(Ht / Theig) - 1) {
+                            drawTransparentImage(ImgTable[7], ImgOutput, sw, sh)
+                        } else {
+                            drawTransparentImage(ImgTable[4], ImgOutput, sw, sh)
+                        }
+                    } else if (hj > 0 && hj < Math.floor(Ht / Theig) - 1) {
+                        if (wi == 0) {
+                            drawTransparentImage(ImgTable[3], ImgOutput, sw, sh)
+                        } else if (wi == Math.floor(Wh / Twidt) - 1) {
+                            drawTransparentImage(ImgTable[5], ImgOutput, sw, sh)
+                        } else {
+                            drawTransparentImage(ImgTable[4], ImgOutput, sw, sh)
+                        }
+                    } else {
+                        drawTransparentImage(ImgTable[4], ImgOutput, sw, sh)
+                    }
+                }
+            }
+        }
+        return ImgOutput
+    }
+}
